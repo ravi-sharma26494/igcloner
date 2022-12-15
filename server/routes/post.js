bc19eb1e14f2d39d06b1488d7799020a1abe9940
Ravi Sharma <ravi.sharma26494@gmail.com>
2022-12-15 16:25:08 +0530

    Context||show elements of navabar a/c to login

--- server/routes/post.js
+++ server/routes/post.js
@@ -9,28 +9,27 @@ router.get('/allpost', (req,res)=>{
     .populate('postedBy', "_id name")
     .then((posts)=>{
         res.json({posts})
-    }).catch((err)=> console.log(err)) 
+    }).catch((error)=> console.log(error)) 
 })
 
 router.post('/createpost',requireLogin,(req,res)=>{
-    const{title, body} = req.body;
-    //console.log(title, body, req.body.photo)
-    //return;
-    // console.log(req.body)
-    //console.log(req.body.title);
-    if(!req.body.photo){
-        return res.status(422).json({err: "Upload and Image to post"});
+    const{title, body, pic} = req.body;
+    console.log(req.body);
+    console.log(`This is your data from request: ${req.body}`)
+    //debugger
+    if(!title || !body || !pic){
+        return res.status(422).json({error: "Please Upload an Image to add your post"});
     }
     req.user.password = undefined;
     const post = new Post({
         title,
         body,
-        photo:req.body.photo,
+        photo:pic,
         postedBy: req.user 
     })
     post.save().then((result)=>{
         res.status(200).json({post: result})
-    }). catch((err)=> console.log(err))
+    }). catch((error)=> console.log(error))
 })
 
 //all the posts by the logged in user
@@ -38,7 +37,7 @@ router.post('/createpost',requireLogin,(req,res)=>{
     Post.find({postedBy: req.user._id}).populate('postedBy','name _id')
     .then((mypost)=>{
         res.status(200).json({mypost})
-    }).catch(err=> console.log(err))
+    }).catch(error=> console.log(error))
  })
 
 module.exports = router;
